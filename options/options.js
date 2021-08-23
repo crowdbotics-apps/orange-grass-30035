176fd69d21cc644d60b0f7fe7dc068197c240ec8
Crowdbotics <team@crowdbotics.com>
2021-08-23 07:29:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "orange_grass_30035Identifier",
+  url: "https://orange_grass_30035Identifier.botics.co",
+  api: "https://orange_grass_30035Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
